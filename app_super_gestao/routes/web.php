e187bd53e94bca9a8ab1856ae31ee307233bfd15
Danilo <daniloworkdev@gmail.com>
2022-05-11 17:48:16 +0100

    redirecionamento rotas

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -42,6 +42,20 @@ route::prefix('/app')->group(function(){
 });
 
 
+Route::get('/rota1', function() {
+    echo 'Rota 1';
+})->name('site.rota1');
+
+
+//Route::redirect('/rota2','/rota1');
+
+
+Route::get('/rota2', function() {
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+
+
 
 
 
