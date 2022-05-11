a3f06ad8cae055e74dd6506ba653d17cf3111130
Danilo <daniloworkdev@gmail.com>
2022-05-11 18:58:25 +0100

    unless blade

--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -25,17 +25,34 @@
     {{ $fornecedores }} não suporta variavel do tipo array
 
     @dd($fornecedores)
+
+    @if(count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados</h3>
+    @elseif(count($fornecedores) > 10)
+        <h3>Existem vários fornecedores cadastrados</h3>
+    @else
+        <h3>Ainda não existem fornecedores cadastrados</h3>
+    @endif
+
+    Enquanto o IF executa se o retorno for true
+    o @unless executa se o retorno for false
     */
 
 
 
     
 @endphp
-
-@if(count($fornecedores) > 0 && count($fornecedores) < 10)
-    <h3>Existem alguns fornecedores cadastrados</h3>
-@elseif(count($fornecedores) > 10)
-    <h3>Existem vários fornecedores cadastrados</h3>
-@else
-    <h3>Ainda não existem fornecedores cadastrados</h3>
+<br/>
+<br/>
+Fornecedor: {{ $fornecedores[0]['nome'] }}
+<br/>
+Status: {{ $fornecedores[0]['status'] }}
+<br/>
+@if( !($fornecedores[0]['status'] == 'S') )
+    Fornecedor inativo
 @endif
+<br/>
+@unless($fornecedores[0]['status'] == 'S') <!---Ele executa se o retorno da condiçã for false diferente do IF que é quando true -->
+    Fornecedor inativo
+@endunless
+<br/>
