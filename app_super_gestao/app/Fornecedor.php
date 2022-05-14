3d44c0d362ce8022035b6e955be4b29e8c1c8ee1
Danilo <daniloworkdev@gmail.com>
2022-05-14 12:34:29 +0100

    Tinker + Eloquent, Migration - Status, Reset, Refresh e Fresh

--- app_super_gestao/app/Fornecedor.php
+++ app_super_gestao/app/Fornecedor.php
@@ -4,7 +4,11 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+//fornecedors Eloquent apenas acrescenta o S no final e pode dar conflitos.
+//fornecedores
+
 class Fornecedor extends Model
 {
-    //
+    // Atributo table vai sempre sobrepor a nomeação automatica do eloquent
+    protected $table = 'fornecedores';
 }
--- app_super_gestao/app/SiteContato.php
+++ app_super_gestao/app/SiteContato.php
@@ -4,6 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+// Como o Eloquent faz a conversão de nome
+// Site_Contato primeira conversão laravel faz para identificar o nome da tebela no banco
+// site_contato deixando tudo minusculo
+// site_contatos por fim adicionando s no final
+// Modelo no singular Tabelas no plural
+
 class SiteContato extends Model
 {
     //
