b91eb022b834dea674f36b1cd78d2c985a3f2ac8
Danilo <daniloworkdev@gmail.com>
2022-05-15 10:18:55 +0100

    Eloquent estudo completo

--- app_super_gestao/app/Fornecedor.php
+++ app_super_gestao/app/Fornecedor.php
@@ -3,13 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 //fornecedors Eloquent apenas acrescenta o S no final e pode dar conflitos.
 //fornecedores
 
 class Fornecedor extends Model
 {
+    use SoftDeletes;
     // Atributo table vai sempre sobrepor a nomeação automatica do eloquent
     protected $table = 'fornecedores';
     protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
+
