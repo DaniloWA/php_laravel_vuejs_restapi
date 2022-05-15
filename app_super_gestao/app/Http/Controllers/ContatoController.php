d92554ee0ae203798a6796af429069a9008c1cec
Danilo <daniloworkdev@gmail.com>
2022-05-16 00:26:42 +0100

    Refactoring usando base de dados

--- app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\SiteContato;
+use App\MotivoContato;
 
 class ContatoController extends Controller
 {
@@ -37,11 +38,7 @@ class ContatoController extends Controller
         //$contato->save();
         //print_r($contato->getAttributes());
         */
-        $motivo_contatos= [
-            '1' => 'Dúvida',
-            '2' => 'Elogio',
-            '3' => 'Reclamação',
-        ];
+        $motivo_contatos = MotivoContato::all();
 
 
         return view('site.contato', ['titulo' => 'Contato', 'motivo_contatos' => $motivo_contatos]);
