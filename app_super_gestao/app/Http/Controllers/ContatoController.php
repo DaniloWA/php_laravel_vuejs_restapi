4090579832c1204bd2c4f7464a870a61df8d694e
Danilo <daniloworkdev@gmail.com>
2022-05-15 12:28:39 +0100

    Request + insert dados BD

--- app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -3,12 +3,55 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function contato(){
+    public function contato(Request $request){
+        /*
+            echo '<pre>';
+            print_r($request->all());
+            echo '</pre>';
+            echo $request->input('nome');
+            echo ' <br> ';
+            echo $request->input('email');
+        
 
-        var_dump($_POST);
+        $contato = new SiteContato();
+        $contato->nome = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivo_contato = $request->input('motivo_contato');
+        $contato->mensagem = $request->input('mensagem');
+
+        //print_r($contato->getAttributes());
+                $contato->save();
+        
+
+        //preenche o objeto com um array associativo de maneira pratica mas precisa o atributop fillable 
+        //$contato->fill($request->all());
+        $contato = new SiteContato();
+
+        //Cria diretamente um objeto baseado em um array associativo! des de que assim como fill a variavel protect $fillable esteja definida de modo a permitir o insert em massa das informações e assim não precisando do metodo ->save() como no fill()
+        $contato->create($request->all());
+        //$contato->save();
+        //print_r($contato->getAttributes());
+        */
+
+
         return view('site.contato', ['titulo' => 'Contato']);
+        
+    }
+
+    public function salvar(Request $request){   
+         //:: executa o metodo create sem a necessidade de instanciar o objeto $contato = new SiteContato();   
+        
+
+        // Realizar a validação dos dados do formulário recebidos no request
+        $request->validate([
+            'nome' => ''
+        ]);
+        // SiteContato::create($request->all());
     }
 }
+
--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -32,7 +32,7 @@ Abaixo com controller */
 Route::get('/', 'PrincipalController@principal')->name('site.index');
 Route::get('/sobre-nos', 'SobreNosController@sobre')->name('site.sobrenos');
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
-Route::post('/contato', 'ContatoController@contato')->name('site.contato');
+Route::post('/contato', 'ContatoController@salvar')->name('site.contato');
 Route::get('/login', function(){return 'Login';})->name('site.login');
 
 
