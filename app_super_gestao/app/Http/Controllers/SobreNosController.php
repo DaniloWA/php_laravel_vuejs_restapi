f2e8db94ccb9822a9d5acaf6a79c74c9417c63f3
Danilo <daniloworkdev@gmail.com>
2022-05-18 18:03:03 +0100

    Middleware no controller

--- app_super_gestao/app/Http/Controllers/SobreNosController.php
+++ app_super_gestao/app/Http/Controllers/SobreNosController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Middleware\logAcessoMiddleware;
 
 class SobreNosController extends Controller
 {
+    //Adicionando um Middleware diretamentem em um constroller invez de por na rota 
+    public function __construct(){
+        $this->middleware(logAcessoMiddleware::class);
+    }
     public function sobre(){
         return view('site.sobre-nos');
     }
