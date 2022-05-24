2385f7c9e81c93cad2857efe9ecf7a5fca445fa2
Danilo <daniloworkdev@gmail.com>
2022-05-24 17:37:19 +0100

    lazy loading e Eager loading 2

--- app_super_gestao/resources/views/app/produto_detalhe/edit.blade.php
+++ app_super_gestao/resources/views/app/produto_detalhe/edit.blade.php
@@ -17,14 +17,15 @@
         </div>
 
         <div class="informacao-pagina">
+            <!-- {{ $produtos_detalhe->toJson() }} -->
             <h4>Produto</h4>
             <div>Nome: {{ $produtos_detalhe->produto->nome }}</div>
+            <!-- {{ $produtos_detalhe->toJson() }} -->
             <br>
             <div>Descrição: {{ $produtos_detalhe->produto->descricao }}</div>
 
             <div style="width: 30%; margin-left: auto; margin-right: auto;" >
                 @component('app.produto_detalhe._components.form_create_edit',['produtos_detalhe' => $produtos_detalhe, 'unidades' => $unidades])
-                    
                 @endcomponent
             </div>
         </div>
