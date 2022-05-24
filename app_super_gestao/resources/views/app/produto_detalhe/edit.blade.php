cc6b937b1e63b8e32968ee0964e2dbd2f446de7c
Danilo <daniloworkdev@gmail.com>
2022-05-24 15:23:56 +0100

    eloquent 1 para 1 belongsTo

--- app_super_gestao/resources/views/app/produto_detalhe/edit.blade.php
+++ app_super_gestao/resources/views/app/produto_detalhe/edit.blade.php
@@ -17,6 +17,11 @@
         </div>
 
         <div class="informacao-pagina">
+            <h4>Produto</h4>
+            <div>Nome: {{ $produtos_detalhe->produto->nome }}</div>
+            <br>
+            <div>Descrição: {{ $produtos_detalhe->produto->descricao }}</div>
+
             <div style="width: 30%; margin-left: auto; margin-right: auto;" >
                 @component('app.produto_detalhe._components.form_create_edit',['produtos_detalhe' => $produtos_detalhe, 'unidades' => $unidades])
                     
