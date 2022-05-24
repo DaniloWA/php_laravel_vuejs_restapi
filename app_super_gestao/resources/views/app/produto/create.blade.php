3c948396d1ee8b44bc9c12616b7215a6f9014faa
Danilo <daniloworkdev@gmail.com>
2022-05-24 08:35:34 +0100

    refactoring edit add produto 2

--- app_super_gestao/resources/views/app/produto/create.blade.php
+++ app_super_gestao/resources/views/app/produto/create.blade.php
@@ -23,32 +23,8 @@
 
         <div class="informacao-pagina">
             <div style="width: 30%; margin-left: auto; margin-right: auto;" >
-                @if(isset($produto->id))
-                    <form action="{{ route('produto.update', ['produto' => $produto->id])}}" method="POST">
-                        @csrf
-                        @method('PUT')
-                @else
-                    <form action="{{ route('produto.store')}}" method="POST">
-                        @csrf
-                @endif
-                <input type="text"  name="nome" value="{{ $produto->nome ?? old('nome') }}" placeholder="Nome" class="borda-preta">
-                {{ $errors->has('nome') ? $errors->first() : ''}}
-                <input type="text" value="{{ $produto->descricao ?? old('descricao') }}" name="descricao" placeholder="Descrição" class="borda-preta">
-                {{ $errors->has('descricao') ? $errors->first() : ''}}
-                <input type="text" value="{{ $produto->peso ??  old('peso') }}" name="peso" placeholder="Peso" class="borda-preta">
-                {{ $errors->has('peso') ? $errors->first() : ''}}
-
-               <select name="unidade_id">
-                   <option value="">-- Selecione a Unidade de Medida --</option>
-                   @foreach($unidades as $unidade)
-                        <option value="{{$unidade->id}}" {{($produto->unidade_id ?? old('unidade_id') == $unidade->id ? 'selected' : '')}} >{{$unidade->descicao}}</option>
-                   @endforeach
-
-                   </select>
-                   {{ $errors->has('unidade_id') ? $errors->first() : ''}}
-
-                    <button type="submit" class="borda-preta">Cadastrar</button>
-                </form>
+                @component('app.produto._components.form_create_edit', ['unidades' => $unidades])                    
+                @endcomponent
             </div>
         </div>
 
--- app_super_gestao/resources/views/app/produto/edit.blade.php
+++ app_super_gestao/resources/views/app/produto/edit.blade.php
@@ -19,27 +19,9 @@
 
         <div class="informacao-pagina">
             <div style="width: 30%; margin-left: auto; margin-right: auto;" >
-                <form action=" {{ route('produto.update', ['produto' => $produto->id]) }} " method="POST">
-                    @csrf
-                    @method('PUT')
-                    <input type="text"  name="nome" value="{{ $produto->nome ?? old('nome') }}" placeholder="Nome" class="borda-preta">
-                    {{ $errors->has('nome') ? $errors->first() : ''}}
-                    <input type="text" value="{{ $produto->descricao ?? old('descricao') }}" name="descricao" placeholder="Descrição" class="borda-preta">
-                    {{ $errors->has('descricao') ? $errors->first() : ''}}
-                    <input type="text" value="{{ $produto->peso ??  old('peso') }}" name="peso" placeholder="Peso" class="borda-preta">
-                    {{ $errors->has('peso') ? $errors->first() : ''}}
-
-                   <select name="unidade_id">
-                       <option value="">-- Selecione a Unidade de Medida --</option>
-                       @foreach($unidades as $unidade)
-                            <option value="{{$unidade->id}}" {{($produto->unidade_id ?? old('unidade_id') == $unidade->id ? 'selected' : '')}} >{{$unidade->descicao}}</option>
-                       @endforeach
-
-                   </select>
-                   {{ $errors->has('unidade_id') ? $errors->first() : ''}}
-
-                    <button type="submit" class="borda-preta">Editar</button>
-                </form>
+                @component('app.produto._components.form_create_edit',['produto' => $produto, 'unidades' => $unidades])
+                    
+                @endcomponent
             </div>
         </div>
 
