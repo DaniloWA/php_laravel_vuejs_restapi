1fa00647772ef98c8e791290d5a7235f73b1e9e8
Danilo <daniloworkdev@gmail.com>
2022-05-26 18:57:37 +0100

    feat: novos dados de produto

--- app_super_gestao/resources/views/app/pedido_produto/create.blade.php
+++ app_super_gestao/resources/views/app/pedido_produto/create.blade.php
@@ -23,8 +23,10 @@
 
         <div class="informacao-pagina">
             <div style="width: 30%; margin-left: auto; margin-right: auto;" >
+                <h4>Detalhes do pedido</h4>
+                <p>ID do pedido: {{ $pedido->id }}</p>
+                <p>Cliente: {{ $pedido->cliente_id }}</p>
                 @component('app.pedido_produto._components.form_create', ['pedido' => $pedido, 'produtos' => $produtos])
-                    
                 @endcomponent
             </div>
         </div>
